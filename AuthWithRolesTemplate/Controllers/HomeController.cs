883178b72cb6c3a68381ba169c3f7db9c40c050a
DostonAbdunazarov <abdunazarov.doston@mail.ru>
2022-11-17 15:41:16 +0500

    add authorization and authentication with roles

--- AuthWithRolesTemplate/Controllers/HomeController.cs
+++ AuthWithRolesTemplate/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using AuthWithRolesTemplate.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -18,6 +19,7 @@ namespace AuthWithRolesTemplate.Controllers
             return View();
         }
 
+        [Authorize]
         public IActionResult Privacy()
         {
             return View();
--- AuthWithRolesTemplate/Data/ApplicationDbContext.cs
+++ AuthWithRolesTemplate/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using AuthWithRolesTemplate.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace AuthWithRolesTemplate.Data
@@ -8,6 +9,21 @@ namespace AuthWithRolesTemplate.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+        public DbSet<AppUser> AppUser { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<AppUser>()
+                .Ignore(p => p.RoleId);
+
+            builder.Entity<AppUser>()
+                .Ignore(p => p.Role);
+
+            builder.Entity<AppUser>()
+                .Ignore(p => p.RoleList);
         }
     }
 }
